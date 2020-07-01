161fc48b04179276f674088880938968dd39cc49
Mike Williamson <michael@dikuw.com>
2020-07-01 08:04:33 +0700

    added routes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const db = require('./database');
+const router = require('./routes');
+
+const app = express();
+
+require('dotenv').config({ path: 'variables.env' });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+    res.send('This is the Hely Cosmetics website backend/API');
+})
+
+app.set('port', process.env.PORT || 8000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running on port ${server.address().port}`);
+});
