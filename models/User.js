387267d752462781a29c625636872342d8b8399e
Mike Williamson <michael@dikuw.com>
2020-07-02 12:01:08 +0700

    added user model

---
+++ models/User.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Please enter a valid email'],
+    required: 'Please enter an email address'
+  },
+  name: {
+    type: String,
+    required: 'Please enter a name',
+    trim: true
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  userType: {
+    type: String,
+    enum: ['held', 'keyholder'],
+    required: 'Please select a user type',
+    default: 'held'
+  },
+  image: {
+    type: String,
+    default: "default.png"
+  },
+  timezone: {
+    type: String,
+    required: 'Please select a timezone'
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  SSID: {
+    type: String,
+    trim: true
+  },
+  password: {
+    type: String,
+    trim: true
+  },
+  is18: {
+    type: Boolean,
+    default: true
+  },
+  added: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
