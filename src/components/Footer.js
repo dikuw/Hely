72afc013f06ac99d2a101e12ee0cfe4983eba1b4
Mike Williamson <michael@dikuw.com>
2020-07-06 11:15:43 +0700

    dev dev dev

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <div>
+        This is the Footer
+      </div>
+    )
+  }
+}
+
+export default Footer;
