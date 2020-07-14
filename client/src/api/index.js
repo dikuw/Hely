bd6e7780efd824864ec1373b1949bb546007f361
Mike Williamson <michael@dikuw.com>
2020-07-14 16:25:37 +0700

    fixed SSL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://helybe.herokuapp.com/api',
+    baseURL: 'https://helybe.herokuapp.com/',
     // baseURL: 'http://localhost:8000/api',
 });
 
