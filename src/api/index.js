6cf174d3913bad7357e26a2282ab1164afcdfb05
Mike Williamson <michael@dikuw.com>
2020-07-14 16:31:33 +0700

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://helybe.herokuapp.com/',
+    baseURL: 'https://helybe.herokuapp.com/api',
     // baseURL: 'http://localhost:8000/api',
 });
 
