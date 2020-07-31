a59cbbec80f32d047e22fb543f0396913efa7ecd
Mike Williamson <michael@dikuw.com>
2020-07-31 18:23:57 +0700

    test

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
+const validator = require('validator');
+// const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
   email: {
@@ -8,12 +11,13 @@ const userSchema = new Schema({
     unique: true,
     lowercase: true,
     trim: true,
+    validate: [validator.isEmail, 'Please enter a valid email'],
     required: 'Please enter an email address'
   },
   name: {
     type: String,
-    trim: true,
-    required: 'Please enter a name'
+    required: 'Please enter a name',
+    trim: true
   },
   resetPasswordToken: String,
   resetPasswordExpires: Date,
@@ -22,5 +26,8 @@ const userSchema = new Schema({
     default: false
   },
 }, { timestamps: true });
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+// userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model('User', userSchema);
